692c63d41a8e739d3b28a5cbd74e6c9fdc4bddec
Маканов Нуржан Сабырович <makanov_n@cpi.nis.edu.kz>
2024-12-30 17:07:13 +0500

    initial commit

---
+++ src/core/core.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { IS_DEV_ENV } from '../shared/utills/is-dev.utills';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { getGraphQLConfig } from './config/graphql.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV,
+      isGlobal: true
+    }),
+    GraphQLModule.forRootAsync({
+      driver: ApolloDriver,
+      imports: [ConfigModule],
+      useFactory: getGraphQLConfig,
+      inject: [ConfigService]
+    }),
+    PrismaModule
+  ]
+
+})
+export class CoreModule { }
